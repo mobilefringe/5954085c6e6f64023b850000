c774e1447134ecea36f5e1175f8ec200649f1128
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-29 09:15:48 -0400

    Fair Lakes 1.2  /site.js

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-var site_json = {
+var site_json ={
                     "mall_name" : "Fair Lakes",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/580f9dab6e6f643917000000/image/jpeg/1497286652000/FC.jpg",
                     "time_zone" : "US/Eastern",
