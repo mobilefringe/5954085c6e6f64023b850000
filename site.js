6354657216a20701a71fcb16c390c22d16cbc800
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-29 10:17:41 -0400

    Fair Lakes 1.2  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json ={
                     "mall_name" : "Fair Lakes",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/580f9dab6e6f643917000000/image/jpeg/1497286652000/FC.jpg",
+                    "default_image" : "//placehold.it/600x600",
                     "time_zone" : "US/Eastern",
                     "newsletter_form_url" : "https://mobilefringe.createsend.com/t/d/s/cpdih/?callback=?",
                     "newsletter_email_field" : "cm-cpdih-cpdih"
